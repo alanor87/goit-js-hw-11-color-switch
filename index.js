e0bca6c10188a372a22be7c4d4eabcf1035c999b
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-02-21 20:50:36 +0200

    commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const startBtnRef = document.querySelector('[data-action="start"]');
+const stopBtnRef = document.querySelector('[data-action="stop"]');
+const bodyRef = document.querySelector('body');
+
+startBtnRef.addEventListener('click', timerHandler);
+stopBtnRef.addEventListener('click', timerHandler);
+
+function colorChange() {
+    bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+}
+
+function timerHandler(event) {
+    switch (event.target.dataset.action) {
+        case 'start':
+            startBtnRef.disabled = true;
+            timer = setInterval(colorChange, 1000);
+            break;
+        case 'stop':
+            startBtnRef.disabled = false;
+            clearInterval(timer);
+            break;
+    }
+}
